2af9150428edfeba429b7ed9663777cb828f62c2
ryan941 <shihyi.chiu@gmail.com>
2022-11-15 00:17:38 +0800

    tailwindcss ok

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import NavbarVue from './components/Navbar.vue';
 </script>
 
 <template>
-  <h1>app</h1>
-  <div>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">about</RouterLink>
-  </div>
-  <br />
+  <NavbarVue />
   <RouterView />
 </template>
 
